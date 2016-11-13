e9f79e49d59a16e5dd54fe358997baeb5499f01a
AkiyoshiOkano <akiyoshiokano0415@gmail.com>
2016-11-14 00:27:43 +0900

    Q4

--- programming_contest.rb
+++ programming_contest.rb
@@ -24,7 +24,7 @@ n = 3
 m = 10
 k = [1, 3, 5]
 
-solve1(n,m,k)
+#solve1(n,m,k)
 
 def solve2(n, a)
   answer = 0
@@ -49,15 +49,15 @@ end
 n = 5
 a = [2, 3, 4, 5, 10]
 
-solve2(n,a)
+#solve2(n,a)
 
 def solve3(length, ants)
-  all_minT = 0;
-  all_maxT = 0;
+  all_minT = 0
+  all_maxT = 0
 
   ants.each do |i|
-  	distA = i
-  	distB = length - i
+    distA = i
+    distB = length - i
 
     ant_minT = [distA ,distB].min
     all_minT = ant_minT if ant_minT > all_minT
@@ -74,9 +74,58 @@ l = 10
 n = 3
 x = [2, 6, 7]
 
-solve3(l, x)
+#solve3(l, x)
 
 
+def binaly_search(x,k,n)
+  l = 0
+  r = n
+
+  while r - l >= 1 do
+    i = (l + r) / 2
+    if k[i] == x
+      return true
+    elsif k[i] < x
+      l = i + 1
+    else
+      r = i
+    end
+  end
+
+  return false
+end
+
+def solve4(n,m,k)
+
+  k.sort!
+
+  f = false
+
+  for a in 0..(n-1) do
+    for b in 0..(n-1) do
+      for c in 0..(n-1) do
+        x = m - k[a] - k[b] - k[c]
+        if binaly_search(x, k, n)
+          f = true
+          pair = "[#{k[a]}, #{k[b]}, #{k[c]}, #{x}]のとき、和は#{m}"
+        end
+      end
+    end
+  end
+
+  puts "Q4 ハードルが上がった「くじびき」"
+  if f == true
+    puts "A. Yes, #{pair}"
+  else
+    puts "A. No"
+  end
+end
+
+n = 3
+m = 10
+k = [1, 3, 5]
+
+#solve4(n,m,k)
 
 
 
